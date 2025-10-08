078adc3b7dd150199cbe378051682af8b108a82d
andreamusso96 <andreamusso96@gmail.com>
2025-10-08 15:48:38 +0200

    Added documentation for us raw

--- src/orchestration/defs/assets/usa_sources.py
+++ src/orchestration/defs/assets/usa_sources.py
@@ -33,6 +33,7 @@ def _copy_table_from_duckdb_to_postgres(table_duckdb: str, table_postgres: str, 
     pool="duckdb_write"
 )
 def usa_hist_census_place_population(context: dg.AssetExecutionContext, duckdb: DuckDBResource, postgres: PostgresResource, tables: TableNamesResource):
+    """Population of historical census places from the Census Place Project. Same as the census_place_population asset in the ipums_full_count defs."""
     context.log.info(f"Copying census place population from duckdb to postgres")
     _copy_table_from_duckdb_to_postgres(
         table_duckdb=tables.names.usa.ipums_full_count.census_place_population(),
@@ -49,6 +50,8 @@ def usa_hist_census_place_population(context: dg.AssetExecutionContext, duckdb: 
     pool="duckdb_write"
 )
 def usa_hist_census_place_migration(context: dg.AssetExecutionContext, duckdb: DuckDBResource, postgres: PostgresResource, tables: TableNamesResource):
+    """Migration between historical census places from the Census Place Project. Same as the census_place_migration asset in the ipums_full_count defs."""
+
     context.log.info(f"Copying census place migration from duckdb to postgres")
     _copy_table_from_duckdb_to_postgres(
         table_duckdb=tables.names.usa.ipums_full_count.census_place_migration(),
@@ -62,9 +65,18 @@ def usa_hist_census_place_migration(context: dg.AssetExecutionContext, duckdb: D
     deps=[nhgis_place_population_1990_2020_downloaded],
     kinds={'postgres'},
     group_name="usa_raw",
-    io_manager_key="postgres_io_manager"
+    io_manager_key="postgres_io_manager",
+    metadata={
+        "dagster/column_schema": dg.TableSchema([
+            dg.TableColumn(name="GISJOIN", type="STR", description="Standard identifiers to join with boundary files"),
+            dg.TableColumn(name="DATAYEAR", type="INT", description="Year of the data"),
+            dg.TableColumn(name="CL8AA", type="INT", description="Population of the census place"),
+        ])
+    }
 )
 def usa_nhgis_census_place_population_1990_2020_raw(context: dg.AssetExecutionContext, storage: StorageResource):
+    """Population of NHGIS census places between 1990 and 2020"""
+
     context.log.info(f"Copying census place population from nhgis to postgres")
     data = pd.read_csv(storage.paths.usa.nhgis.census_place_pop_1990_2020(), encoding='latin1')
     return data
@@ -72,9 +84,18 @@ def usa_nhgis_census_place_population_1990_2020_raw(context: dg.AssetExecutionCo
 @dg.asset(
     deps=[nhgis_place_geom_1900_2010_downloaded],
     kinds={'postgres'},
-    group_name="usa_raw"
+    group_name="usa_raw",
+    metadata={
+        "dagster/column_schema": dg.TableSchema([
+            dg.TableColumn(name="NHGISPLACE", type="INT", description="The NHGIS ID of the census place"),
+            dg.TableColumn(name="PLACE", type="STR", description="The name of the census place"),
+            dg.TableColumn(name="geometry", type="GEOGRAPHY", description="The geometry of the census place (an point)"),
+        ])
+    }
 )
 def usa_nhgis_census_place_geom_all_years_raw(context: dg.AssetExecutionContext, postgres: PostgresResource, storage: StorageResource, tables: TableNamesResource):
+    """Geometry of NHGIS census places between 1900 and 2010"""
+
     context.log.info(f"Copying census place geom from nhgis to postgres for year")
     census_place_geom_gdf_list = []
     for year in range(1900, 2011, 10):
@@ -96,9 +117,17 @@ def usa_nhgis_census_place_geom_all_years_raw(context: dg.AssetExecutionContext,
     deps=[raw_data_zenodo],
     kinds={'postgres'},
     group_name="usa_raw",
-    io_manager_key="postgres_io_manager"
+    io_manager_key="postgres_io_manager",
+    metadata={
+        "dagster/column_schema": dg.TableSchema([
+            dg.TableColumn(name="id", type="INT", description="The Census Place Project ID of the census place"),
+            dg.TableColumn(name="lon", type="FLOAT", description="The longitude of the census place"),
+            dg.TableColumn(name="lat", type="FLOAT", description="The latitude of the census place"),
+        ])
+    }
 )
 def usa_hist_census_place_geom_raw(context: dg.AssetExecutionContext, storage: StorageResource):
+    """Geometry of historical census places from the Census Place Project"""
     context.log.info(f"Copying census place geom from hist to postgres")
     census_place_geom_df = pd.read_csv(storage.paths.usa.census_place_project.hist_census_place_geom())
     return census_place_geom_df
@@ -107,9 +136,17 @@ def usa_hist_census_place_geom_raw(context: dg.AssetExecutionContext, storage: S
 @dg.asset(
     deps=[raw_data_zenodo],
     kinds={'postgres'},
-    group_name="usa_raw"
+    group_name="usa_raw",
+    metadata={
+        "dagster/column_schema": dg.TableSchema([
+            dg.TableColumn(name="State_Code", type="STR", description="The two letter code of the state (e.g. CA, NY)"),
+            dg.TableColumn(name="State_Name", type="STR", description="The name of the state (e.g. California, New York)"),
+            dg.TableColumn(name="geometry", type="GEOGRAPHY", description="The geometry of the state"),
+        ])
+    }
 )
 def usa_states_geom_raw(context: dg.AssetExecutionContext, postgres: PostgresResource, storage: StorageResource, tables: TableNamesResource):
+    """Geometry of the states of the USA"""
     context.log.info(f"Copying states geom from hist to postgres")
     states_geom_gdf = gpd.read_file(storage.paths.usa.misc.usa_state_geom())
     states_geom_gdf.to_postgis(
