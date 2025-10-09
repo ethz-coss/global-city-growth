585bf427b39ccc569f58362d0bd06001bcc09c7b
andreamusso96 <andreamusso96@gmail.com>
2025-10-09 17:36:02 +0200

    Replaced partitions with loops in IPUMS full count

--- src/orchestration/defs/assets/ipums_full_count.py
+++ src/orchestration/defs/assets/ipums_full_count.py
@@ -9,8 +9,6 @@ from .constants import constants
 from .download import ipums_usa_full_count_downloaded, raw_data_zenodo
 
 IPUMS_HISTORICAL_YEARS = constants["IPUMS_HISTORICAL_YEARS"]
-ipums_historical_years_partitions = dg.StaticPartitionsDefinition([str(y) for y in IPUMS_HISTORICAL_YEARS])
-
 
 def _execute_sql_query_and_return_num_rows(duckdb: DuckDBResource, sql_query: str, table_name: str):
     with duckdb.get_connection() as con:
@@ -36,7 +34,6 @@ def _load_table_into_duckdb(duckdb: DuckDBResource, table_name: str, file_path: 
 @dg.asset(
     deps=[ipums_usa_full_count_downloaded],
     kinds={'duckdb'},
-    partitions_def=ipums_historical_years_partitions,
     group_name="ipums_full_count_staging",
     pool="duckdb_write",
     metadata={
@@ -48,18 +45,20 @@ def _load_table_into_duckdb(duckdb: DuckDBResource, table_name: str, file_path: 
 )
 def ipums_full_count_raw(context: dg.AssetExecutionContext, duckdb: DuckDBResource, storage: StorageResource, tables: TableNamesResource):
     """IPUMS full count data for a given year (raw)."""
-    year = context.partition_key
-    table_name = tables.names.usa.ipums_full_count.ipums_full_count_raw(year)
-    file_path = storage.paths.usa.ipums_full_count.ipums_full_count(year)
+    num_rows = 0
+    for year in IPUMS_HISTORICAL_YEARS:
+        table_name = tables.names.usa.ipums_full_count.ipums_full_count_raw(year)
+        file_path = storage.paths.usa.ipums_full_count.ipums_full_count(year)
 
-    context.log.info(f"Loading IPUMS full count data for {year} into table {table_name}")
-    num_rows = _load_table_into_duckdb(duckdb, table_name, file_path)
-    return dg.Output(value=table_name, metadata={"num_rows": num_rows, "table": table_name})
+        context.log.info(f"Loading IPUMS full count data for {year} into table {table_name}")
+        num_rows_year = _load_table_into_duckdb(duckdb, table_name, file_path)
+        num_rows += num_rows_year
+    
+    return dg.Output(value=num_rows, metadata={"num_rows": num_rows})
 
 @dg.asset(
     deps=[ipums_full_count_raw],
     kinds={'duckdb'},
-    partitions_def=ipums_historical_years_partitions,
     group_name="ipums_full_count_intermediate",
     pool="duckdb_write",
     metadata={
@@ -71,30 +70,31 @@ def ipums_full_count_raw(context: dg.AssetExecutionContext, duckdb: DuckDBResour
 )
 def ipums_full_count_clean(context: dg.AssetExecutionContext, duckdb: DuckDBResource, tables: TableNamesResource):
     """IPUMS full count data for a given year (clean)."""
-    year = context.partition_key
-    raw_table_name = tables.names.usa.ipums_full_count.ipums_full_count_raw(year)
-    clean_table_name = tables.names.usa.ipums_full_count.ipums_full_count_clean(year)
+    num_rows = 0
+    for year in IPUMS_HISTORICAL_YEARS:
+        raw_table_name = tables.names.usa.ipums_full_count.ipums_full_count_raw(year)
+        clean_table_name = tables.names.usa.ipums_full_count.ipums_full_count_clean(year)
+        
+        context.log.info(f"Cleaning IPUMS full count data for {year} into table {clean_table_name}")
 
-    context.log.info(f"Cleaning IPUMS full count data for {year} into table {clean_table_name}")
+        sql_query = f"""
+        DROP TABLE IF EXISTS {clean_table_name};
+        CREATE TABLE {clean_table_name} AS
+        SELECT  UPPER(histid) AS histid,
+                NULLIF(TRIM(ANY_VALUE(hik)), '') AS hik
+        FROM { raw_table_name }
+        GROUP BY UPPER(histid)
+        """
 
-    sql_query = f"""
-    DROP TABLE IF EXISTS {clean_table_name};
-    CREATE TABLE {clean_table_name} AS
-    SELECT  UPPER(histid) AS histid,
-            NULLIF(TRIM(ANY_VALUE(hik)), '') AS hik
-    FROM { raw_table_name }
-    GROUP BY UPPER(histid)
-    """
-
-    num_rows = _execute_sql_query_and_return_num_rows(duckdb=duckdb, sql_query=sql_query, table_name=clean_table_name)
-    return dg.Output(value=clean_table_name, metadata={"num_rows": num_rows, "table": clean_table_name})
+        num_rows_year = _execute_sql_query_and_return_num_rows(duckdb=duckdb, sql_query=sql_query, table_name=clean_table_name)
+        num_rows += num_rows_year
+    return dg.Output(value=num_rows, metadata={"num_rows": num_rows})
     
 
 # Define the geographic data asset
 @dg.asset(
     deps=[raw_data_zenodo],
     kinds={'duckdb'},
-    partitions_def=ipums_historical_years_partitions,
     group_name="ipums_full_count_staging",
     pool="duckdb_write",
     metadata={
@@ -106,19 +106,20 @@ def ipums_full_count_clean(context: dg.AssetExecutionContext, duckdb: DuckDBReso
 )
 def crosswalk_hist_id_to_hist_census_place_raw(context: dg.AssetExecutionContext, duckdb: DuckDBResource, storage: StorageResource, tables: TableNamesResource):
     """Crosswalk between full count census hist_id and census place id for a given year (raw)."""
-    year = context.partition_key
-    table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_raw(year)
-    file_path = storage.paths.usa.census_place_project.crosswalk_hist_id_to_hist_census_place(year)
+    num_rows = 0
+    for year in IPUMS_HISTORICAL_YEARS:
+        table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_raw(year)
+        file_path = storage.paths.usa.census_place_project.crosswalk_hist_id_to_hist_census_place(year)
 
-    context.log.info(f"Loading crosswalk data for {year} into table {table_name}")
-    num_rows = _load_table_into_duckdb(duckdb, table_name, file_path)
-    return dg.Output(value=table_name, metadata={"num_rows": num_rows, "table": table_name})
+        context.log.info(f"Loading crosswalk data for {year} into table {table_name}")
+        num_rows_year = _load_table_into_duckdb(duckdb, table_name, file_path)
+        num_rows += num_rows_year
+    return dg.Output(value=num_rows, metadata={"num_rows": num_rows})
 
 
 @dg.asset(
     deps=[crosswalk_hist_id_to_hist_census_place_raw],
     kinds={'duckdb'},
-    partitions_def=ipums_historical_years_partitions,
     group_name="ipums_full_count_intermediate",
     pool="duckdb_write",
     metadata={
@@ -130,28 +131,29 @@ def crosswalk_hist_id_to_hist_census_place_raw(context: dg.AssetExecutionContext
 )
 def crosswalk_hist_id_to_hist_census_place_clean(context: dg.AssetExecutionContext, duckdb: DuckDBResource, tables: TableNamesResource):
     """Crosswalk between full count census hist_id and census place id for a given year (clean)."""
-    year = context.partition_key
-    raw_table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_raw(year)
-    clean_table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_clean(year)
+    num_rows = 0
+    for year in IPUMS_HISTORICAL_YEARS:
+        raw_table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_raw(year)
+        clean_table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_clean(year)
 
-    context.log.info(f"Cleaning crosswalk data for {year} into table {clean_table_name}")
+        context.log.info(f"Cleaning crosswalk data for {year} into table {clean_table_name}")
 
-    sql_query = f"""
-    DROP TABLE IF EXISTS {clean_table_name};
-    CREATE TABLE {clean_table_name} AS
-    SELECT  UPPER(histid) AS histid,
-            ANY_VALUE(cpp_placeid) AS census_place_id
-    FROM { raw_table_name }
-    GROUP BY UPPER(histid)
-    """
+        sql_query = f"""
+        DROP TABLE IF EXISTS {clean_table_name};
+        CREATE TABLE {clean_table_name} AS
+        SELECT  UPPER(histid) AS histid,
+                ANY_VALUE(cpp_placeid) AS census_place_id
+        FROM { raw_table_name }
+        GROUP BY UPPER(histid)
+        """
 
-    num_rows = _execute_sql_query_and_return_num_rows(duckdb=duckdb, sql_query=sql_query, table_name=clean_table_name)
-    return dg.Output(value=clean_table_name, metadata={"num_rows": num_rows, "table": clean_table_name})
+        num_rows_year = _execute_sql_query_and_return_num_rows(duckdb=duckdb, sql_query=sql_query, table_name=clean_table_name)
+        num_rows += num_rows_year
+    return dg.Output(value=num_rows, metadata={"num_rows": num_rows})
 
 @dg.asset(
     deps=[ipums_full_count_clean, crosswalk_hist_id_to_hist_census_place_clean],
     kinds={'duckdb'},
-    partitions_def=ipums_historical_years_partitions,
     group_name="ipums_full_count_intermediate",
     pool="duckdb_write",
     metadata={
@@ -164,31 +166,33 @@ def crosswalk_hist_id_to_hist_census_place_clean(context: dg.AssetExecutionConte
 )  
 def ipums_full_count_census_place_id(context: dg.AssetExecutionContext, duckdb: DuckDBResource, tables: TableNamesResource):
     """IPUMS full count data with census place id for a given year."""
-    year = context.partition_key
-    ipums_full_count_table_name = tables.names.usa.ipums_full_count.ipums_full_count_clean(year)
-    crosswalk_hist_id_to_hist_census_place_table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_clean(year)
-    table_name = tables.names.usa.ipums_full_count.ipums_full_count_census_place_id(year)
+    num_rows = 0
+    for year in IPUMS_HISTORICAL_YEARS:
+        ipums_full_count_table_name = tables.names.usa.ipums_full_count.ipums_full_count_clean(year)
+        crosswalk_hist_id_to_hist_census_place_table_name = tables.names.usa.ipums_full_count.crosswalk_hist_id_to_hist_census_place_clean(year)
+        table_name = tables.names.usa.ipums_full_count.ipums_full_count_census_place_id(year)
 
-    context.log.info(f"Joining IPUMS full count data with census place id for {year}")
+        context.log.info(f"Joining IPUMS full count data with census place id for {year}")
 
-    sql_query = f"""
-    DROP TABLE IF EXISTS {table_name};
-    CREATE TABLE {table_name} AS
-    SELECT  histid,
-            fcc.hik,
-            CASE WHEN cpp.census_place_id > 69491 THEN NULL ELSE cpp.census_place_id END AS census_place_id
-    FROM {ipums_full_count_table_name} AS fcc
-    LEFT JOIN {crosswalk_hist_id_to_hist_census_place_table_name} AS cpp
-    USING (histid)
-    WHERE histid IS NOT NULL
-    """
+        sql_query = f"""
+        DROP TABLE IF EXISTS {table_name};
+        CREATE TABLE {table_name} AS
+        SELECT  histid,
+                fcc.hik,
+                CASE WHEN cpp.census_place_id > 69491 THEN NULL ELSE cpp.census_place_id END AS census_place_id
+        FROM {ipums_full_count_table_name} AS fcc
+        LEFT JOIN {crosswalk_hist_id_to_hist_census_place_table_name} AS cpp
+        USING (histid)
+        WHERE histid IS NOT NULL
+        """
 
-    num_rows = _execute_sql_query_and_return_num_rows(duckdb=duckdb, sql_query=sql_query, table_name=table_name)
-    return dg.Output(value=table_name, metadata={"num_rows": num_rows, "table": table_name})
+        num_rows_year = _execute_sql_query_and_return_num_rows(duckdb=duckdb, sql_query=sql_query, table_name=table_name)
+        num_rows += num_rows_year
+    return dg.Output(value=num_rows, metadata={"num_rows": num_rows})
     
 
 @dg.asset(
-    deps=[dg.AssetDep(ipums_full_count_census_place_id, partition_mapping=dg.AllPartitionMapping())],
+    deps=[ipums_full_count_census_place_id],
     kinds={'duckdb'},
     group_name="ipums_full_count_intermediate",
     pool="duckdb_write",
