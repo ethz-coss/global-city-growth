39b8591b5fd93295fdf50036d5a78701c524a977
andreamusso96 <andreamusso96@gmail.com>
2025-10-10 12:22:12 +0200

    Wrong reference

--- src/warehouse/models/world/si_figures/world_population_share_cities_above_1m_projections_ols.sql
+++ src/warehouse/models/world/si_figures/world_population_share_cities_above_1m_projections_ols.sql
@@ -2,6 +2,6 @@
 
 -- depends_on: {{ ref('world_urban_population') }}
 -- depends_on: {{ source('owid', 'world_country_region') }}
-{% set rank_size_slope_table = source('figure_data_prep', 'world_rank_size_slopes_ols') %}
+{% set rank_size_slope_table = source('figure_data_prep', 'world_rank_size_slopes_historical_ols') %}
 
 {{ get_population_share_cities_above_1m_projections(rank_size_slope_table) }}
--- src/warehouse/models/world/si_figures/world_rank_size_slopes_ols_decade_change.sql
+++ src/warehouse/models/world/si_figures/world_rank_size_slopes_ols_decade_change.sql
@@ -3,7 +3,7 @@ WITH rank_size_slopes_decade_change AS (
             year, 
             analysis_id, 
             LEAD(rank_size_slope) OVER (PARTITION BY country, analysis_id, MOD(year, 10) ORDER BY year) / rank_size_slope - 1 AS rank_size_slope_decade_change
-    FROM {{ source('figure_data_prep', 'world_rank_size_slopes_ols') }}
+    FROM {{ source('figure_data_prep', 'world_rank_size_slopes_historical_ols') }}
 )
 SELECT *
 FROM rank_size_slopes_decade_change
